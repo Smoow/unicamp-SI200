ac495df8439cdc02f790838fbd92e309ddff8c60
Pedro H. C <pedrohcm5@gmail.com>
2020-09-23 15:59:46 -0300

    Update 01.c
    função b) soma_series ok

--- modularizacao-recursividade/01.c
+++ modularizacao-recursividade/01.c
@@ -4,25 +4,27 @@ a) o somatório de 1 até N;
 b) o valor de S, onde S = 1 + 1/2 + 1/3 + ... + 1/N;
 c) o valor de S, onde S = 1 + 1/1! + 1/2! + ... + 1 /N! (faça também uma função
 recursiva para calcular o fatorial de N e utilize na função que calcula S). */
+
 #include <stdio.h>
 
 int somatorio(int num);
-int funcao_b(int num);
+float soma_series(int num);
 
 int main() {
 
     int num;
     printf("Digite um numero: ");
     scanf("%d", &num);
-    printf("O somatorio de 1 ate %d eh %d\n", num, somatorio(num));
-    printf("O valor de S, onde S = 1 + 1/2 + 1/3 + ... + 1/%d eh %f", num, (float) funcao_b(num));
+    printf("O somatorio de 1 ate %d eh %d\n", num, somatorio(num)); // Chamada da função somatorio
+    printf("O valor de S, onde S = 1 + 1/2 + 1/3 + ... + 1/%d eh %f\n", num, soma_series(num)); // Chamada da função soma_series
 
     return 0;
 }
 
-// Funcao a) somatorio
+// Função a) somatorio
 int somatorio(int num) {
 
+    // Recursividade
     if (num == 0) 
         return 0;
     else 
@@ -30,4 +32,15 @@ int somatorio(int num) {
 
 }
 
-//Funcao b)
+// Função b) soma de series
+float soma_series(int num) {
+
+    int S = 0;
+
+    // Recursividade
+    if (num == 1)
+        return 1.0;
+    else
+        return ( (1.0/num) + soma_series(num - 1.0) );
+    
+}

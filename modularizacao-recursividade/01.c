adb2f62dca79ca88d4880bcd8b554a2aaa377806
Pedro H. C <pedrohcm5@gmail.com>
2020-09-23 16:22:46 -0300

    Update 01.c
    clean  - acentos no código

--- modularizacao-recursividade/01.c
+++ modularizacao-recursividade/01.c
@@ -17,51 +17,51 @@ int main() {
     int num;
     printf("Digite um numero: ");
     scanf("%d", &num);
-    printf("O somatorio de 1 ate %d eh %d\n", num, somatorio(num)); // Chamada da função somatorio
-    printf("O valor de S, onde S = 1 + 1/2 + 1/3 + ... + 1/%d eh %f\n", num, soma_series(num)); // Chamada da função soma_series
-    printf("O valor de S, onde S = 1 + 1/2! + 1/3! + ... + 1/%d! eh %f\n", num, soma_series_fat(num)); // Chamada da função soma_series
+    printf("O somatorio de 1 ate %d eh %d\n", num, somatorio(num)); // Chamada da funcao somatorio
+    printf("O valor de S, onde S = 1 + 1/2 + 1/3 + ... + 1/%d eh %f\n", num, soma_series(num)); // Chamada da funcao soma_series
+    printf("O valor de S, onde S = 1 + 1/2! + 1/3! + ... + 1/%d! eh %f\n", num, soma_series_fat(num)); // Chamada da funcao soma_series
 
     return 0;
 }
 
-// Função a) somatorio
+// Funcao a) somatorio
 int somatorio(int num) {
 
     // Recursividade
     if (num == 0) 
-        return 0; // Condição de parada
+        return 0; // Condicao de parada
     else 
         return (num + somatorio(num - 1));
 
 }
 
-// Função b) soma de series
+// Funcao b) soma de series
 float soma_series(int num) {
 
     // Recursividade
     if (num == 1)
-        return 1.0; // Condição de parada
+        return 1.0; // Condicao de parada
     else
         return ( (1.0/num) + soma_series(num - 1.0) );
     
 }
 
-// Função c) soma de series fatorial
+// Funcao c) soma de series fatorial
 float soma_series_fat(int num) {
 
     // Recursividade
     if (num == 1)
-        return 1.0; // Condição de parada
+        return 1.0; // Condicao de parada
     else
         return ( (1.0/fat(num)) + soma_series_fat(num - 1.0) );
     
 }
 
-// Função para calcular o fatorial de um número inteiro positivo
+// Funcao para calcular o fatorial de um número inteiro positivo
 int fat(int num)
 {
     if (num == 0)
-        return 1; // Condição de parada
+        return 1; // Condicao de parada
     else
         return (num * fat(num - 1));
 }

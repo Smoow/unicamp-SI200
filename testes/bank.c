1d97440d480352405fcadc0384a758a88118ea9d
Pedro H. C <pedrohcm5@gmail.com>
2020-10-01 19:03:53 -0300

    Create bank.c

---
+++ testes/bank.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    int choice = 0;
+
+    printf("+++ Bem-vindo ao seu Banco! +++\n\n");
+    printf("$ O que deseja fazer?");
+    printf("1. Ver saldo");
+    printf("2. Depositar");
+    printf("3. Sacar");
+
+    return 0;
+}
